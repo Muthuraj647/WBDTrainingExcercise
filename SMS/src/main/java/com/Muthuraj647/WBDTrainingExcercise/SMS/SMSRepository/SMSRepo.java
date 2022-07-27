c4f8c490fa8f76914d80b6815c047856abefb444
Muthuraj647 <muthurajmuthuraj32@gmail.com>
2022-07-27 21:01:26 +0530

    SMS Module Application Setup

---
+++ SMS/src/main/java/com/Muthuraj647/WBDTrainingExcercise/SMS/SMSRepository/SMSRepo.java
@@ -0,0 +1,7 @@
+package com.Muthuraj647.WBDTrainingExcercise.SMS.SMSRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SMSRepo extends JpaRepository<T, ID> {
+
+}
